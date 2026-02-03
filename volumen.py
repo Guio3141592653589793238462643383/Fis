b71bed96e34da28939653a837176c86b7443ef7b
Andres Guio <pipeguio11@gmail.com>
2026-02-02 23:18:41 -0500

    Actu repo ordenado

--- volumen.py
+++
@@ -1,5 +0,0 @@
-altura=float(input("Ingrese la altura del objeto en cm: "))
-largo=float(input("ingres el largo del objeto en cm: "))
-ancho=float(input("ingrese el ancho del objeto en cm: "))
-volumen=altura*largo*ancho
-print("El volumen del objeto es de:", volumen, "cm³")
--- volumen_mej.py
+++
@@ -1,7 +0,0 @@
-def volumen_mej(h,l,a):
-    return h*l*a
-h=float(input("Ingrese la altura del objeto en m:"))
-l=float(input("Ingrese el largo del objeto en m:"))
-a=float(input("Ingrese el ancho del objeto en m:"))
-Vo=volumen_mej(h,l,a)
-print("El volumen del objeto es de:", Vo, "m³")
