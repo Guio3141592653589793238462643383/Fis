1ccbfd00e7e7592e98bb88cf72eb335d479c0a42
Andres Guio <pipeguio11@gmail.com>
2026-02-24 14:45:44 -0500

    prueba

--- visualizacion/tabular_seno.py
+++ visualizacion/tabular_seno.py
@@ -3,4 +3,5 @@ import numpy as np
 x = np.linspace(0, 2*np.pi, 100 )
 y= np.sin(x) 
 filas=[[val_x, val_y] for val_x, val_y in zip(x,y)]
+
 print(tabulate(filas, headers=["x", "y"], tablefmt="grid"))
